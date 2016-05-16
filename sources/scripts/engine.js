4b96446d97d33fb7d492a27649555fc084109b88
Leon <lowpolygonum@gmail.com>
2016-05-16 18:58:56 +0200

    boids

---
+++ sources/scripts/engine.js
@@ -0,0 +1,137 @@
+
+	var Engine = function ()
+	{
+		this.particleContainer;
+		this.boidList;
+
+		this.vectorNear;
+		this.vectorAvoid;
+		this.vectorGlobal;
+		this.vectorTarget;
+
+		this.init = function ()
+		{
+			this.particleContainer = new PIXI.ParticleContainer(10000, { scale: true, position: true, rotation: true, uvs: true, alpha: true });
+			this.boidList = [];
+
+			this.vectorNear = Tool.vec2(0,0);
+			this.vectorAvoid = Tool.vec2(0,0);
+			this.vectorGlobal = Tool.vec2(0,0);
+			this.vectorTarget = Tool.vec2(0,0);
+
+
+			for (var i = 0; i < 50; i++)
+			{
+				var boid = PIXI.Sprite.fromImage('../images/toto.png');
+
+				boid.anchor.set(0.5, 1)
+				boid.scale.set(0.25)
+				boid.size = 64
+				boid.target = Tool.vec2(window.innerWidth / 2, window.innerHeight / 2)
+
+				var velocityAngle = Math.random() * Math.PI * 2
+				boid.velocity = Tool.vec2(Math.cos(velocityAngle), Math.sin(velocityAngle))
+
+				boid.speed = Settings.DEFAULT_SPEED
+				boid.friction = Settings.DEFAULT_FRICTION
+				boid.frictionCollision = Settings.DEFAULT_FRICTION_COLLISION
+
+				boid.targetScale = Settings.DEFAULT_TARGET_SCALE
+				boid.avoidScale = Settings.DEFAULT_AVOID_SCALE
+				boid.nearScale = Settings.DEFAULT_NEAR_SCALE
+				boid.globalScale = Settings.DEFAULT_GLOBAL_SCALE
+
+				boid.index = i;
+
+				var self = boid;
+
+				boid.move = function(moveX, moveY)
+				{
+					this.velocity.x += moveX
+					this.velocity.y += moveY
+
+					this.x += this.velocity.x * this.speed / Math.max(1, this.size)
+					this.y += this.velocity.y * this.speed / Math.max(1, this.size)
+
+					this.velocity.x *= this.friction
+					this.velocity.y *= this.friction
+				}
+
+				boid.rumble = function ()
+				{
+					var randomAngle = Math.random() * Math.PI * 2
+					this.velocity.x += Math.cos(randomAngle)
+					this.velocity.y += Math.sin(randomAngle)
+				}
+
+				this.boidList.push(boid);
+				this.particleContainer.addChild(boid);
+			}
+		}
+
+		this.update = function ()
+		{
+			for (var current = 0; current < this.boidList.length; ++current)
+			{
+				var boid = this.boidList[current]
+
+				this.vectorNear.x = this.vectorNear.y = 0
+				this.vectorGlobal.x = this.vectorGlobal.y = 0
+				this.vectorAvoid.x = this.vectorAvoid.y = 0
+				this.vectorTarget.x = boid.target.x - boid.x
+				this.vectorTarget.y = boid.target.y - boid.y
+
+				var globalCount = 0
+				var nearCount = 0
+				for (var other = 0; other < this.boidList.length; ++other) {
+					if (current != other) {
+						var boidOther = this.boidList[other]
+
+						// Avoid
+						var distance = Tool.distance(boid.x, boid.y, boidOther.x, boidOther.y)
+						var dist = distance - (boid.size + boidOther.size)
+						if (dist < Settings.COLLISION_BIAS)
+						{
+							var avoidX = boid.x - boidOther.x
+							var avoidY = boid.y - boidOther.y
+							var angle = Math.atan2(avoidY, avoidX)
+							this.vectorAvoid.x += Math.cos(angle) * Math.max(distance, boid.size + boidOther.size)
+							this.vectorAvoid.y += Math.sin(angle) * Math.max(distance, boid.size + boidOther.size)
+						}
+						// Follow Near
+						if (dist < 30)
+						{
+							this.vectorNear.x += boidOther.velocity.x
+							this.vectorNear.y += boidOther.velocity.y
+							++nearCount
+						}
+						// Follow Global
+						this.vectorGlobal.x += boidOther.x
+						this.vectorGlobal.y += boidOther.y
+						++globalCount
+					}
+				}
+
+				if (globalCount != 0) {
+					this.vectorGlobal.x = (this.vectorGlobal.x / globalCount - boid.x) * boid.globalScale
+					this.vectorGlobal.y = (this.vectorGlobal.y / globalCount - boid.y) * boid.globalScale
+				}
+
+				if (nearCount != 0) {
+					this.vectorNear.x = (this.vectorNear.x / nearCount - boid.x) * boid.nearScale
+					this.vectorNear.y = (this.vectorNear.y / nearCount - boid.y) * boid.nearScale
+				}
+
+				// Scale them
+				this.vectorAvoid.x *= boid.avoidScale
+				this.vectorAvoid.y *= boid.avoidScale
+				this.vectorTarget.x *= boid.targetScale
+				this.vectorTarget.y *= boid.targetScale
+
+				// Apply to Boid
+				boid.move(
+					this.vectorTarget.x + this.vectorNear.x + this.vectorGlobal.x + this.vectorAvoid.x,
+					this.vectorTarget.y + this.vectorNear.y + this.vectorGlobal.y + this.vectorAvoid.y)
+			}
+		}
+	}
---
+++ sources/scripts/settings.js
@@ -0,0 +1,52 @@
+var Settings = {}
+
+Settings.MIN_SIZE = 8
+Settings.MAX_SIZE = 16
+Settings.SYMBOL_COUNT_TO_JUMP = 20
+Settings.MIN_BUBBLE = 8
+Settings.MAX_BUBBLE = 16
+Settings.COLORNESS_SPEED = 0.05
+
+// Boid ratio rules
+Settings.DEFAULT_TARGET_SCALE = 0.1
+Settings.DEFAULT_AVOID_SCALE = 0.7
+Settings.DEFAULT_GLOBAL_SCALE = 0.0001
+Settings.DEFAULT_NEAR_SCALE = 0.01
+
+Settings.THINKER_AVOID_SCALE = 0.05
+
+// Boid condition rules
+Settings.MIN_DIST_TO_FOLLOW = 30
+Settings.MIN_DIST_TO_ABSORB = 20
+Settings.COLLISION_BIAS = 1
+Settings.DEFAULT_FRICTION = 0.9
+Settings.DEFAULT_FRICTION_COLLISION = 0.9
+
+// Boid animation
+Settings.DEFAULT_SPEED = 0.3
+Settings.ORBIT_SCALE = 10
+Settings.ORBIT_SPEED = 0.2
+Settings.TRANSITION_IMPULSE_SCALE = 60
+Settings.TRANSITION_UPDATE_SCALE = 100
+Settings.BOOGIE_SCALE = 0.1
+Settings.BOOGIE_SPEED = 15
+
+// Boid display
+Settings.BUBBLE_OUTLINE = 2
+Settings.LETTER_FONT_SCALE = 2
+Settings.FONT_SIZE = 32
+Settings.FONT_NAME = 'dk_liquid_embraceregular'
+
+Settings.GAME_STATE_INTRO = 0
+Settings.GAME_STATE_PLAY = 1
+Settings.GAME_STATE_OVER = 2
+Settings.GAME_STATE_TRANSITION = 3
+
+Settings.STATE_APPEARING = 0
+Settings.STATE_STANDING = 1
+Settings.STATE_DISAPPEARING = 2
+
+Settings.defaultSpawnDelay = 10
+
+Settings.defaultVolumeSound = 0.5
+Settings.defaultVolumeMusic = 0.25
